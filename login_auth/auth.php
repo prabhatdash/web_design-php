3db12fb47bca1f97d4359ff2b89b74ac83e52e8a
Prabhat Das <prabhatdash0376@gmail.com>
2024-11-14 10:09:24 +0530

    Session Management

--- login_auth/auth.php
+++ login_auth/auth.php
@@ -2,15 +2,31 @@
 
 $email=$_POST['email'];
 
-$password=$_POST["password"];
+$pas=$_POST["password"];
+
+include_once "../update_delete_data/conn.php";
+
+$sql="select email,pass from users where email='$email' and pass='$pas'";
 
 
-if($email=="prabhat@gmail.com" && $password=="12345"){
-    header("location:dashboard.php");
+$results=mysqli_query($conn,$sql);
+
+$data=mysqli_fetch_all($results);
+
+
+if($email==$data[0][0] && $pas==$data[0][1]){
+    session_start();
+    $_SESSION['user']=$email;
+    header("location: ../update_delete_data/index.php");
 }
 else{
-    header("location:index.php?code=404");
-}
+    echo "
+    <script>
+    alert('Invalid Username or Password')
+    window.location.href='index.php';
+    </script>
+    ";
 
+}
 ?>
 
